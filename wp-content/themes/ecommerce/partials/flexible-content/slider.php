f11554f1ac2a17e6fe2926b50e4ee95cdce042d5
Ajay Prajapati <ajayprazz@gmail.com>
2020-01-10 16:19:47 +0545

    updated banner

--- wp-content/themes/ecommerce/partials/flexible-content/slider.php
+++ wp-content/themes/ecommerce/partials/flexible-content/slider.php
@@ -1,6 +1,7 @@
 <section class="slider full-width-slider">
     <div class="slider-item">
-        <div class="slider-image bck-cover" style="background-image: url('../../assets/images/banner-1.jpg')">
+        <div class="slider-image bck-cover"
+             style="background-image: url('<?= get_template_directory_uri() ?>/assets/images/banner-1.jpg')">
         </div>
         <div class="slider-content text-center">
             <span class="slider-title__sm text-uppercase h5">Beauty Pressed Power</span>
@@ -9,7 +10,8 @@
         <a href="#" class="absolute-link"></a>
     </div>
     <div class="slider-item">
-        <div class="slider-image bck-cover" style="background-image: url('assets/images/banner-1.jpg')">
+        <div class="slider-image bck-cover"
+             style="background-image: url('<?= get_template_directory_uri() ?>/assets/images/banner-1.jpg')">
         </div>
         <div class="slider-content text-center">
             <span class="slider-title__sm text-uppercase h5">shop today</span>
@@ -25,7 +27,7 @@
 			<?php
 /*			while ( have_rows( 'slider' ) ):
 				the_row();
-				*/?>
+				*/ ?>
                 <li data-transition="zoomout"
                     data-slotamount="default" data-hideafterloop="0"
                     data-hideslideonmobile="off" data-easein="Power4.easeInOut" data-easeout="Power4.easeInOut"
@@ -34,13 +36,13 @@
                     data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8=""
                     data-param9=""
                     data-param10="" data-description="">
-                    <a href="<?/*= ( get_sub_field( 'is_external' ) ) ? get_sub_field( 'link_url' ) : get_sub_field( 'page_link' ) */?>"
+                    <a href="<? /*= ( get_sub_field( 'is_external' ) ) ? get_sub_field( 'link_url' ) : get_sub_field( 'page_link' ) */ ?>"
                        class="absolute-link"></a>
 					<?php
 /*					$image = wp_get_attachment_image_url( get_sub_field( 'image' ), 'full' );
 					$image = getResizedImage( $image );
-					*/?>
-                    <img src="<?/*= $image['orig'] */?>" alt=""
+					*/ ?>
+                    <img src="<? /*= $image['orig'] */ ?>" alt=""
                          data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat"
                          data-bgparallax="10" class="rev-slidebg" data-no-retina>
                     <div class="tp-caption  tp-resizeme"
@@ -61,11 +63,11 @@
                          data-paddingbottom="[0,0,0,0]"
                          data-paddingleft="[0,0,0,0]"
                          style="line-height: 22px; font-weight: bold; text-transform: uppercase; color: #000; z-index: 999">
-						<?/*= get_sub_field( 'title' ) */?>
+						<? /*= get_sub_field( 'title' ) */ ?>
                     </div>
 					<?php
 /*					if ( get_sub_field( 'sub_title' ) ) :
-						*/?>
+						*/ ?>
                         <div class="tp-caption tp-resizeme"
                              data-x="['left','left','left','center']"
                              data-hoffset="['100','100','70','0']"
@@ -83,15 +85,15 @@
                              data-paddingright="[0,0,0,0]"
                              data-paddingbottom="[0,0,0,0]"
                              data-paddingleft="[0,0,0,0]"
-                             style="font-family: Lora; color: #000; line-height: 60px; text-align: center; text-transform: uppercase; z-index: 999; font-weight: 600"><?/*= get_sub_field( 'sub_title' ) */?>
+                             style="font-family: Lora; color: #000; line-height: 60px; text-align: center; text-transform: uppercase; z-index: 999; font-weight: 600"><? /*= get_sub_field( 'sub_title' ) */ ?>
                         </div>
 					<?php
 /*					endif;
-					*/?>
+					*/ ?>
                 </li>
 			<?php
 /*			endwhile;
-			*/?>
+			*/ ?>
         </ul>
         <div class="tp-bannertimer" style="height: 7px; background-color: rgba(255, 255, 255, 0.25);"></div>
     </div>
--- wp-content/themes/ecommerce/woocommerce/single-product/product-image.php
+++ wp-content/themes/ecommerce/woocommerce/single-product/product-image.php
@@ -60,8 +60,6 @@ if ( empty( $image_ids ) ) {
 			endforeach;
 			?>
         </div>
-        
-
     </div>
     <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12">
         <div class="b-display-list-wrapper">
