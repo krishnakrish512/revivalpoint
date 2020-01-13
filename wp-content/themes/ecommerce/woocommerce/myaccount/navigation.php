12eecfebe1fd091cbda91950f5072f32f28f1ef6
Suman Maharjan <sumanmaharj@gmail.com>
2020-01-13 16:36:42 +0545

    changes

--- wp-content/themes/ecommerce/woocommerce/myaccount/navigation.php
+++ wp-content/themes/ecommerce/woocommerce/myaccount/navigation.php
@@ -22,8 +22,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 do_action( 'woocommerce_before_account_navigation' );
 ?>
 
-<nav class="woocommerce-MyAccount-navigation">
-	<ul>
+<nav class="woocommerce-MyAccount-navigation pr-md-5">
+	<ul class="list-unstyled">
 		<?php foreach ( wc_get_account_menu_items() as $endpoint => $label ) : ?>
 			<li class="<?php echo wc_get_account_menu_item_classes( $endpoint ); ?>">
 				<a href="<?php echo esc_url( wc_get_account_endpoint_url( $endpoint ) ); ?>"><?php echo esc_html( $label ); ?></a>
