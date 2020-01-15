58b255985bb1217a305b909e9379cdacc057b2d4
Suman Maharjan <sumanmaharj@gmail.com>
2020-01-15 16:21:36 +0545

    mobile filter

--- wp-content/themes/ecommerce/header.php
+++ wp-content/themes/ecommerce/header.php
@@ -4,7 +4,7 @@
 
     <!-- META-DATA -->
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
     <meta name="description" content="">
     <meta name="keywords" content="">
 
