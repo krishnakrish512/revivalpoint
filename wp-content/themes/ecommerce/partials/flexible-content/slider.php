f3da1cca32c746bf76a37c0a8ed8dcd3e70da809
krishna512 <krishnakrish512@gmail.com>
2020-12-15 19:40:35 +0545

    added banner files

--- wp-content/themes/ecommerce/partials/flexible-content/slider.php
+++ wp-content/themes/ecommerce/partials/flexible-content/slider.php
@@ -1,26 +1,34 @@
 <section class="slider full-width-slider">
-	<?php
-	while ( have_rows( 'slider' ) ):
-		the_row();
-		?>
+    <?php
+    while (have_rows('slider')):
+        the_row();
+        ?>
         <div class="slider-item">
             <div class="slider-image">
-				<?php
-				$image = wp_get_attachment_image_url( get_sub_field( 'image' ), 'full' );
-				$image = getResizedImage( $image );
-				//				echo \NextGenImage\getWebPHTML( $image['webp'], $image['orig'] );
-				?>
+                <?php
+                $image = wp_get_attachment_image_url(get_sub_field('image'), 'full');
+                $image = getResizedImage($image);
+                //				echo \NextGenImage\getWebPHTML( $image['webp'], $image['orig'] );
+                ?>
                 <img src="<?= $image['orig'] ?>" alt="">
             </div>
+
             <div class="slider-content text-center">
-                <span class="slider-title__sm text-uppercase h5"><?= get_sub_field( 'title' ) ?></span>
-                <h2 class="slider-title__lg text-uppercase"><?= get_sub_field( 'sub_title' ) ?></h2>
+                <?php if ( get_sub_field('link_url') || get_sub_field('page_link')): ?>
+                    <a href="<?= (get_sub_field('is_external')) ? get_sub_field('link_url') : get_sub_field('page_link') ?>">
+                        <button type="button" class="text-uppercase"><?= get_sub_field('button_title') ?></button>
+                    </a>
+                <?php endif; ?>
+                                <span class="slider-title__sm text-uppercase h5"><?= get_sub_field( 'title' )
+               ?></span>
+                                <h2 class="slider-title__lg text-uppercase"><?= get_sub_field( 'sub_title' )
+                ?>
             </div>
-            <a href="<?= ( get_sub_field( 'is_external' ) ) ? get_sub_field( 'link_url' ) : get_sub_field( 'page_link' ) ?>"
+            <a href="<?= (get_sub_field('is_external')) ? get_sub_field('link_url') : get_sub_field('page_link') ?>"
                class="absolute-link"></a>
         </div>
-	<?php
-	endwhile;
-	?>
+    <?php
+    endwhile;
+    ?>
 </section>
 
