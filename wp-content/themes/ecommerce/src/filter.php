9da2c0e4f3d3ad6f238cf4b94b5165586666fec4
krishna512 <krishnakrish512@gmail.com>
2021-01-12 17:40:19 +0545

    changed filter  files

--- wp-content/themes/ecommerce/src/filter.php
+++ wp-content/themes/ecommerce/src/filter.php
@@ -15,11 +15,25 @@ function ecommerce_widget_tag_cloud_args( $args ) {
 
 	$my_args = array(
 		'smallest' => '14',
-		'unit'     => 'px'
+		'unit'     => 'px',
+        'orderby'  => 'count',
+        'order'    => 'DESC',
+        'number'   => 10
 	);
 	$args    = wp_parse_args( $args, $my_args );
-
 	return $args;
 }
 
 add_filter( 'widget_tag_cloud_args', 'ecommerce_widget_tag_cloud_args' );
+
+
+//function set_widget_tag_cloud_args($args) {
+//    $my_args = array(
+//        'orderby'  => 'count',
+//        'order'    => 'DESC',
+//        'number'   => 10
+//    );
+//    $args = wp_parse_args( $args, $my_args );
+//    return $args;
+//}
+//add_filter('woocommerce_product_tag_cloud_widget_args','set_widget_tag_cloud_args');
